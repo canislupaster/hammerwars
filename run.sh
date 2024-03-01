341678685f653eed6561d01d657bdfda55db67d1
thomasqm <thomas@engineeringvirtue.net>
2024-03-01 00:11:04 -0500

    oof

---
+++ run.sh
@@ -0,0 +1,14 @@
+# create logrotate configuration that limits size and directory in /var
+sudo -s -- <<EOF
+touch /etc/logrotate.d/hammerwars
+echo "/var/log/hammerwars/*.log {
+    size 100M
+    rotate 3
+}" > /etc/logrotate.d/hammerwars
+mkdir -p /var/log/hammerwars
+chown -R $USER:$USER /var/log/hammerwars
+chmod -R u+rw /var/log/hammerwars
+EOF
+export $(cat .env | xargs)
+# run screen to log to /var/log/hammerwars
+screen -S hammerwars -L -Logfile /var/log/hammerwars/hammerwars.log -dm ./gradlew run
